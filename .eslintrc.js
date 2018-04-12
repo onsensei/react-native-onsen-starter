7cbc06dcf859fcea9caff02f076d2216b7904218
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-04-12 15:21:29 +0700

    config eslint rule based on notetaker tutorial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   "env": {
     "es6": true,
-    "node": true
+    "node": true,
+    "browser": true
   },
-  "extends": "eslint:recommended",
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
@@ -12,24 +16,95 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react"
+    "react",
+    "react-native",
+    "sort-imports-es6-autofix",
+    "eslint-plugin-import"
   ],
   "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-unused-styles": 2,
+    "react/no-did-mount-set-state": 2,
+    "react/no-did-update-set-state": 2,
+    "react/no-direct-mutation-state": 2,
+    "react/jsx-uses-vars": 2,
+    "react/no-unused-prop-types": 2,
+    "no-unused-vars": [2, {
+      "varsIgnorePattern": "React"
+    }],
+    "comma-spacing": [2, {
+     "before": false,
+     "after": true }],
+     "sort-imports-es6-autofix/sort-imports-es6": [2, {
+      "ignoreCase": true,
+      "ignoreMemberSort": false,
+      "memberSyntaxSortOrder": ["single", "multiple", "all", "none"]
+    }],
+    "import/no-unresolved": [2, {commonjs: true, ignore: ["node_modules"]}],
+    "import/named": [2, {commonjs: false}],
+    "import/newline-after-import": 2,
+    "no-undef": 2,
+    "semi": 2,
+    "react/prop-types": 2,
+    "react/jsx-no-bind": 2,
+    "react/jsx-no-duplicate-props": 2,
+    "space-before-blocks": 2,
+    "space-before-function-paren": 2,
+    "space-in-parens": 2,
+    "space-infix-ops": 2,
+    "space-unary-ops": 2,
+    "spaced-comment": 2,
+    "rest-spread-spacing": 2,
+    "semi-spacing": 2,
+    "no-unneeded-ternary": 2,
+    "eqeqeq": 2,
+    "dot-location": [2, "property"],
+    "no-extra-bind": 2,
+    "keyword-spacing": 2,
+    "key-spacing": 2,
+    "indent": [2, 2],
+    "react/jsx-indent": [2, 2],
+    "func-call-spacing": 2,
+    "array-bracket-spacing": 2,
+    "block-spacing": 2,
+    "brace-style": 2,
+    "arrow-body-style": 2,
+    "arrow-parens": 2,
+    "arrow-spacing": 2,
+    "react/self-closing-comp": 2,
+    "jsx-quotes": [2, "prefer-single"],
+    "object-curly-spacing": 2,
+    "quotes": [2, "single"],
+    "no-console": 2,
+    "comma-dangle": 2,
+    "prefer-const": 2,
+    "no-duplicate-imports": 2,
+    "no-whitespace-before-property": 2,
+    "no-useless-rename": 2,
+    "no-unreachable": 2,
+    "camelcase": 2,
+    "no-multiple-empty-lines": ["error", { "max": 1 }]
+  },
+  "settings": {
+    "import/ignore": [".util","node_modules"], 
+    "import/core-modules": ["resolveAssetSource"] 
+  },
+  "globals": {
+    "GLOBAL": false,
+    "__DEV__": false,
+    "global": false,
+    "it": false,
+    "xit": false,
+    "expect": false,
+    "describe": false,
+    "jest": false,
+    "require": false,
+    "module": false,
+    "Promise": false,
+    "beforeAll": false,
+    "beforeEach": false,
+    "afterAll": false,
+    "afterEach": false
   }
 };

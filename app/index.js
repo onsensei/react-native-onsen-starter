10ddeb35df0c6666540c296b58d00b762b884b74
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-03 10:50:41 +0700

    comment other nav bar

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import Router from './routes/index';
+// import RouterDrawer from './routes/indexDrawer';
+// import RouterStack from './routes/indexStack';
+// import RouterTab from './routes/indexTab';
 
 class App extends Component {
   render () {

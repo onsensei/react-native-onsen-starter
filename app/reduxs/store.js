21b13350556afa920c9266dd030df82a86e558ee
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-07 15:54:26 +0700

    setup redux (root files)

---
+++ app/reduxs/store.js
@@ -0,0 +1,19 @@
+import {compose, createStore/* , applyMiddleware*/} from 'redux';
+// import someReduxMiddleware from 'some-redux-middleware';
+// import someOtherReduxMiddleware from 'some-other-redux-middleware';
+import rootReducer from '../reducers/root.reducer';
+
+const enhancerList = [];
+const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
+
+if (typeof devToolsExtension === 'function') {
+  enhancerList.push(devToolsExtension());
+}
+
+const composedEnhancer = compose(/* applyMiddleware(someReduxMiddleware, someOtherReduxMiddleware),*/ ...enhancerList);
+
+const initStore = () => createStore(rootReducer, {}, composedEnhancer);
+
+module.exports = {
+  initStore
+};

be07b35c0d9db44c265e66aebb566e3be95fd6bc
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-08 11:36:16 +0700

    export action types

--- app/redux/actions/index.action.js
+++ app/redux/actions/index.action.js
@@ -1,9 +1,9 @@
 import {createAction} from 'redux-actions';
  
 // type
-const GOOD_MORNING_ACTION = 'GOOD_MORNING_ACTION';
-const GOOD_AFTERNOON_ACTION = 'GOOD_AFTERNOON_ACTION';
-const GOOD_NIGHT_ACTION = 'GOOD_NIGHT_ACTION';
+export const GOOD_MORNING_ACTION = 'GOOD_MORNING_ACTION';
+export const GOOD_AFTERNOON_ACTION = 'GOOD_AFTERNOON_ACTION';
+export const GOOD_NIGHT_ACTION = 'GOOD_NIGHT_ACTION';
 
 // action
 export const greetingMorning = createAction(GOOD_MORNING_ACTION);

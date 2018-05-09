5c054beb5bc04f93a0deb909837383d0b2e8c7f4
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-09 14:59:55 +0700

    use redux middleware to edit action.payload

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,7 +1,10 @@
-import {compose, createStore/* , applyMiddleware*/} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 // import someReduxMiddleware from 'some-redux-middleware';
 // import someOtherReduxMiddleware from 'some-other-redux-middleware';
 import rootReducer from './reducers/index.reducer';
+import * as actions from './actions/index.action';
+
+// setup
 
 const enhancerList = [];
 const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
@@ -10,6 +13,18 @@ if (typeof devToolsExtension === 'function') {
   enhancerList.push(devToolsExtension());
 }
 
-const composedEnhancer = compose(/* applyMiddleware(someReduxMiddleware, someOtherReduxMiddleware),*/ ...enhancerList);
+// middleware
+
+const greetingMiddleware = () => (next) => (action) => {
+  if (action.type === actions.GOOD_NIGHT_ACTION) {
+    action.payload = 'Sweet Dream';
+  }
+
+  next(action);
+};
+
+const composedEnhancer = compose(applyMiddleware(greetingMiddleware), ...enhancerList);
+
+// store
 
 export const initStore = () => createStore(rootReducer, {}, composedEnhancer);

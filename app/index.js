9ac717fb72cc0c2b99e2d27ddd8f4ab70ba80853
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-11 14:40:37 +0700

    setup redux router

--- app/index.js
+++ app/index.js
@@ -1,31 +1,50 @@
+import noop from 'lodash/noop';
+import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import Router from './routes/index';
 // import RouterDrawer from './routes/indexDrawer';
 // import RouterStack from './routes/indexStack';
 // import RouterTab from './routes/indexTab';
+import {addNavigationHelpers} from 'react-navigation';
+import {connect, Provider} from 'react-redux';
 import {initStore} from './redux/store';
-import {Provider} from 'react-redux';
 
-class App extends Component {
+// redux router
+
+class ReduxRouter extends Component {
   render () {
-    return <Router/>;
+    const {dispatch, nav} = this.props;
+    const navigation = addNavigationHelpers({dispatch, state: nav});
+    return <Router navigation={navigation} />;
   }
 }
 
-// export default App;
+ReduxRouter.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  nav: PropTypes.object.isRequired
+};
 
-// ----------
+ReduxRouter.defaultProps = {
+  dispatch: noop,
+  nav: null
+};
+
+const mapStateToProps = (state) => ({nav: state.nav});
+
+const ConnectedRouter = connect(mapStateToProps)(ReduxRouter);
+
+// app (store + redux router)
 
 const store = initStore();
 
-class ProvidedApp extends Component {
+class App extends Component {
   render () {
     return (
       <Provider store={store}>
-        <App />
+        <ConnectedRouter />
       </Provider>
     );
   }
 }
 
-export default ProvidedApp;
+export default App;

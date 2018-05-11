c784aa5ed62b984e63dc72f1b608c8ad4e90abb7
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-11 11:38:43 +0700

    add VectorIcon page for nav drawer

--- app/routes/indexDrawer.js
+++ app/routes/indexDrawer.js
@@ -3,6 +3,7 @@ import Detail from '../components/Detail/Detail.component';
 import Icon from 'react-native-vector-icons/Foundation';
 import React from 'react';
 import Setting from '../components/Setting/Setting.component';
+import VectorIcon from '../components/VectorIcon/VectorIcon.component';
 import {DrawerNavigator, StackNavigator} from 'react-navigation';
 
 const getRootHomeNavOpt01 = ({navigation}) => {
@@ -74,6 +75,29 @@ const SettingStackNavigator = StackNavigator({
   }
 });
 
+const getRootHomeNavOpt04 = ({navigation}) => {
+  const onPressMenuButton = () => {
+    navigation.navigate('DrawerToggle');
+  };
+  
+  const MenuButton = ( // eslint-disable-next-line
+    <Icon.Button name='list' color='black' iconStyle={{padding: 10}} backgroundColor='transparent' onPress={onPressMenuButton}/>
+  );
+
+  return {
+    headerTitle: 'VectorIcon Nav',
+    headerLeft: MenuButton,
+    drawerLabel: 'VectorIcon Drawer'
+  };
+};
+
+const VectorIconStackNavigator = StackNavigator({
+  VectorIcon: {
+    screen: VectorIcon,
+    navigationOptions: getRootHomeNavOpt04
+  }
+});
+
 // ----------
 
 const HomeDrawerNavigator = DrawerNavigator({
@@ -85,6 +109,9 @@ const HomeDrawerNavigator = DrawerNavigator({
   },
   Setting: {
     screen: SettingStackNavigator
+  },
+  VectorIcon: {
+    screen: VectorIconStackNavigator
   }
 });
 

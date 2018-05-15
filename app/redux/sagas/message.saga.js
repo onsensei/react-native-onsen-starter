f8ace7431e4c7f91d50444cd8fd78b15d9c31b13
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-15 14:43:21 +0700

    remove unused code

--- app/redux/sagas/message.saga.js
+++ app/redux/sagas/message.saga.js
@@ -3,18 +3,6 @@ import * as actions from './../../redux/actions/index.action';
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-export function* goodMorningHandler () {
-  //
-}
-
-export function* goodAfternoonHandler () {
-  //
-}
-
-export function* goodNightHandler () {
-  //
-}
-
 export function* greetingAllHandler () {
   yield put(actions.goodMorningActionCreator({
     message: 'Good Morning from saga',
@@ -36,9 +24,6 @@ export function* greetingAllHandler () {
 }
 
 function* messageSaga () {
-  yield takeLatest(actions.GOOD_MORNING_ACTION, goodMorningHandler);
-  yield takeLatest(actions.GOOD_AFTERNOON_ACTION, goodAfternoonHandler);
-  yield takeLatest(actions.GOOD_NIGHT_ACTION, goodNightHandler);
   yield takeLatest(actions.GREETING_ALL_ACTION, greetingAllHandler);
 }
 

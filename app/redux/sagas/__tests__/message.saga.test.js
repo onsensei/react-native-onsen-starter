aa4677384300c9a5d6fb37597da62037182b1829
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-05-15 15:44:31 +0700

    add test case for message saga

---
+++ app/redux/sagas/__tests__/message.saga.test.js
@@ -0,0 +1,69 @@
+import sagaHelper from 'redux-saga-testing';
+import {call, put, takeLatest} from 'redux-saga/effects';
+import * as actions from './../../../redux/actions/index.action';
+import * as MessageSaga from '../message.saga';
+
+describe('MessageSaga.default', () => {
+  const it = sagaHelper(MessageSaga.default());
+
+  it('should takeLatest greetingAllHandler', (result) => {
+    expect(result).toEqual(takeLatest(actions.GREETING_ALL_ACTION, MessageSaga.greetingAllHandler));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('MessageSaga.greetingAllHandler', () => {
+  const it = sagaHelper(MessageSaga.greetingAllHandler());
+
+  it('should call delay', (result) => {
+    expect(result).toEqual(call(MessageSaga.delay, MessageSaga.defaultDelay));
+  });
+
+  it('should put actions.goodMorningActionCreator with params', (result) => {
+    // arrange
+    const params = {
+      message: 'Good Morning from saga',
+      time: '8:00'
+    };
+
+    // assert
+    expect(result).toEqual(put(actions.goodMorningActionCreator(params)));
+  });
+
+  it('should call delay', (result) => {
+    expect(result).toEqual(call(MessageSaga.delay, MessageSaga.defaultDelay));
+  });
+
+  it('should put actions.goodAfternoonActionCreator with params', (result) => {
+    // arrange
+    const params = {
+      message: 'Good Afternoon from saga',
+      time: '14:00'
+    };
+
+    // assert
+    expect(result).toEqual(put(actions.goodAfternoonActionCreator(params)));
+  });
+
+  it('should call delay', (result) => {
+    expect(result).toEqual(call(MessageSaga.delay, MessageSaga.defaultDelay));
+  });
+
+  it('should put actions.goodNightActionCreator with params', (result) => {
+    // arrange
+    const params = {
+      message: 'Good Night from saga',
+      time: '20:00'
+    };
+
+    // assert
+    expect(result).toEqual(put(actions.goodNightActionCreator(params)));
+  });
+
+  it('and then nothing', (result) => {
+    expect(result).toBeUndefined();
+  });
+});
--- app/redux/sagas/message.saga.js
+++ app/redux/sagas/message.saga.js
@@ -1,30 +1,31 @@
 import {call, put, takeLatest} from 'redux-saga/effects';
 import * as actions from './../../redux/actions/index.action';
 
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+export const defaultDelay = 1000;
+export const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export function* greetingAllHandler () {
+  yield call(delay, defaultDelay);
   yield put(actions.goodMorningActionCreator({
     message: 'Good Morning from saga',
     time: '8:00'
   }));
-  yield call(delay, 1000);
 
+  yield call(delay, defaultDelay);
   yield put(actions.goodAfternoonActionCreator({
     message: 'Good Afternoon from saga',
     time: '14:00'
   }));
-  yield call(delay, 1000);
-
+  
+  yield call(delay, defaultDelay);
   yield put(actions.goodNightActionCreator({
     message: 'Good Night from saga',
     time: '20:00'
   }));
-  yield call(delay, 1000);
 }
 
-function* messageSaga () {
+function* MessageSaga () {
   yield takeLatest(actions.GREETING_ALL_ACTION, greetingAllHandler);
 }
 
-export default messageSaga;
+export default MessageSaga;
